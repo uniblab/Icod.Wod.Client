a774148f5cb3116c65daec8aada8bf240e6bc9ce
uniblab <uniblab@hotmail.com>
2020-11-06 12:47:26 -0500

    assembly version
    Signed-off-by: uniblab <uniblab@hotmail.com>

---
+++ AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Icod.Wod.Client" )]
+[assembly: AssemblyDescription( "Client program to execute Icod Work on Demand Schematics" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Icod" )]
+[assembly: AssemblyProduct( "Icod.Wod.Client" )]
+[assembly: AssemblyCopyright( "Copyright © 2020, Timothy J. Bruce" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "9FD26799-891E-4546-82E7-977CC8845DB0" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion( "4.0.0" )]
+[assembly: AssemblyInformationalVersion( "4.0.0" )]
+[assembly: AssemblyFileVersion( "4.0.0" )]
